42dbb0b5ac8507d05990f3259fa8416d1fc584cc
mahdi kazemi <mmk90@yahoo.com>
2021-09-11 23:35:48 +0430

    Create ASP.Net Core Project

---
+++ ASPCoreMVC/Data/Migrations/20210911190115_sync.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ASPCoreMVC.Data.Migrations
+{
+    public partial class sync : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ ASPCoreMVC/Models/CleanArchtectureDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPCoreMVC.Models
+{
+    public class CleanArchtectureDBContext:DbContext
+    {
+        public CleanArchtectureDBContext(DbContextOptions<CleanArchtectureDBContext> options) :base(options)
+        {
+
+        }
+    }
+}
