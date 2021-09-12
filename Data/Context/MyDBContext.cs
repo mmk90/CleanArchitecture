008cf05652ede293a2445d32700a0e6f4555cf7d
Mahdi <mmk90@yahoo.com>
2021-09-12 15:08:01 +0430

    Episode 8

---
+++ Data/Context/MyDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data.Context
+{
+    public class MyDBContext:DbContext
+    {
+        public MyDBContext(DbContextOptions<MyDBContext> options):base(options)
+        {
+
+        }
+        public DbSet<Course> Courses { get; set; }
+    }
+}
